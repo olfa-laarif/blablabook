d1a3a14515d27d494f9c7a9e3418a95bdd422406
olfa.laarif <olfa.laarif@gmail.com>
2025-02-12 22:15:22 +0100

    delete nametable from models

--- back/models/mark.model.js
+++ back/models/mark.model.js
@@ -18,7 +18,6 @@ Mark.init({
   }
 }, {
   sequelize,
-  tableName: 'mark',
   indexes: [
     {
       unique: true,
--- back/models/user.model.js
+++ back/models/user.model.js
@@ -43,7 +43,6 @@ User.init({
       }
     },
     sequelize,
-    tableName: 'user'
   });
 
 
