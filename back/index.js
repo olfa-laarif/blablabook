5fb08eda8ef72ab08e0fccef00ffbf8ee650601c
PradoWilder <p.o.maisonneuve@gmail.com>
2025-02-13 19:11:43 +0100

    feature-connexion-back : Use of sanitizeHTML to clean body before crossing the router

--- back/index.js
+++ back/index.js
@@ -2,11 +2,15 @@ import "dotenv/config";
 import express from "express";
 import { router } from "./router.js";
 import cors from "cors";
+import { bodySanitizer } from "./middlewares/sanitize.middleware.js";
 
 // Création de l'app
 export const app = express();
 app.use(cors());
 app.use(express.json());
+
+// utilisation du middleware "sanitizer" ici avant le router afin de nettoyer tous les body qui arrivent au router (en mode NTUI - Never Trust User InputU)
+app.use(bodySanitizer);
 
 app.use(router);
 
