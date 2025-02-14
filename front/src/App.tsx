d76b8c3aa1391ab951315c46e6fd7d8f4c352ff0
olfa.laarif <olfa.laarif@gmail.com>
2025-02-14 13:58:46 +0100

    add page SignUpPage

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import BookDetails from './pages/BookDetails';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 
 
 
@@ -16,6 +17,7 @@ function App() {
       <Route path="/books/:id" element={<BookDetails />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
       </Routes>
       </BrowserRouter>
 
--- front/src/components/Login.tsx
+++ front/src/components/Login.tsx
@@ -2,92 +2,38 @@ import { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
 
 export default function Login() {
-
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
-    
+
   return (
-    <div className="flex items-center justify-center py-8 bg-gray-100">
-      <div className="w-full max-w-md p-6 bg-white rounded-2xl shadow-md mt-30">
-        <h2 className="text-2xl font-bold text-center text-gray-700">
-          Créer un compte
-        </h2>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-6 bg-white rounded-2xl shadow-md ">
+        <h2 className="text-2xl font-bold text-center text-gray-700">Se connecter</h2>
         <form className="mt-4">
           <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">
-              Nom
-            </label>
-            <input
-              type="text" 
-              className="w-full px-4 py-1 mt-1 border border-gray-300  rounded-lg focus:ring focus:ring-indigo-500 focus:outline-none"
-              placeholder="Votre nom"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">
-              Prenom
-            </label>
-            <input
-              type="text"
-              className="w-full px-4 py-1 mt-1 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-500 focus:outline-none"
-              placeholder="Votre prenom"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">
-              Pseudo
-            </label>
-            <input
-              type="text"
-              className="w-full px-4 py-1 mt-1 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-500 focus:outline-none"
-              placeholder="Votre pseudo"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">
-              Email
-            </label>
-            <input
-              type="email"
-              className="w-full px-4 py-1 mt-1 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-500 focus:outline-none"
-              placeholder="Votre email"
-            />
+            <label className="block text-sm font-medium text-gray-600">Email</label>
+            <input type="email" className="w-full px-4 py-1 mt-1 border border-gray-300 border border-gray-300-gray-300 rounded-lg focus:ring focus:ring-indigo-500 focus:outline-none" placeholder="Votre email" />
           </div>
           <div className="mb-4">
             <label className="block text-sm font-medium text-gray-600">Mot de passe</label>
+            {/* Ce div est utilisé pour positionner l'icône de visibilité du mot de passe en mode absolu à l'intérieur du champ de saisie. */}
             <div className="relative">
-              <input type={passwordVisible ? "text" : "password"} className="w-full px-4 py-1 mt-1 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-500 focus:outline-none" placeholder="Votre mot de passe" />
+              {/* Ce champ d'entrée bascule entre un type 'password' et 'text' en fonction de l'état de `passwordVisible`. */}
+              <input type={passwordVisible ? "text" : "password"} className="w-full px-4 py-1 mt-1 border border-gray-300 border border-gray-300-gray-300 rounded-lg focus:ring focus:ring-indigo-500 focus:outline-none" placeholder="Votre mot de passe" />
+              {/* Ce bouton permet d'afficher ou masquer le mot de passe en modifiant l'état `passwordVisible` lorsqu'on clique dessus. */}
               <button
                 type="button"
                 className="absolute right-3 top-3 text-gray-600"
                 onClick={() => setPasswordVisible(!passwordVisible)}
               >
+                {/* Cette expression conditionnelle affiche l'icône d'œil ouvert ou fermé en fonction de `passwordVisible`. */}
                 {passwordVisible ? <EyeOff size={20} /> : <Eye size={20} />}
               </button>
             </div>
           </div>
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">Confirmer votre mot de passe</label>
-            <div className="relative">
-              <input type={confirmPasswordVisible ? "text" : "password"} className="w-full px-4 py-1 mt-1 border border-gray-300 rounded-lg focus:ring focus:ring-indigo-500 focus:outline-none" placeholder="Confirmez votre mot de passe" />
-              <button
-                type="button"
-                className="absolute right-3 top-3 text-gray-600"
-                onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}
-              >
-                {confirmPasswordVisible ? <EyeOff size={20} /> : <Eye size={20} />}
-              </button>
-            </div>
-          </div>
-          <button className="w-full px-4 py-1 font-bold text-white bg-indigo-400 rounded-lg hover:bg-indigo-600">
-            S'inscrire
-          </button>
+          <button className="w-full px-4 py-1 font-bold text-white bg-indigo-400 rounded-lg hover:bg-indigo-600">Se connecter</button>
         </form>
         <p className="mt-4 text-sm text-center text-gray-600">
-          Déjà un compte ?{" "}
-          <a href="#" className="text-indigo-500 hover:underline">
-            Se connecter
-          </a>
+          Pas encore de compte ? <a href="/signup" className="text-indigo-500 hover:underline">S'inscrire</a>
         </p>
       </div>
     </div>
