d84217f56cf07cc5136538da65420ff584ede6b4
olfa.laarif <olfa.laarif@gmail.com>
2025-02-18 13:28:35 +0100

    add  sign up front

--- front/src/services/api.tsx
+++ front/src/services/api.tsx
@@ -2,6 +2,7 @@
 // Obtention de tous les livres (Méthode getAllBooks qui pointera sur une route get '/books')
 // Obtention d'un livre par son ID (Méthode getBookById qui pointera sur la route get'/books/${id})
 // Fetch à faire directement dans HomePage et BookDetails avec useEffect
+//import type { NewUserData } from "../types";
 
 const API_BASE_URL = "http://localhost:3001";
 
@@ -81,6 +82,42 @@ export const getConnectedUser = async () => {
         return null;
     }
 };
+
+
+
+export const  registerUser= async (lastnameFromInput: string,firstnameFromInput: string,usernameFromInput: string,emailFromInput: string,passwordFromInput: string)=>{
+try {
+    const response = await fetch(`${API_BASE_URL}/api/users/register`, 
+    // données et configuration
+    {credentials: "include",
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        username: usernameFromInput,
+        password: passwordFromInput,
+        email: emailFromInput,
+        firstname: firstnameFromInput,
+        lastname: lastnameFromInput,
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error ("Erreur de creation de l'utilisateur ");    
+    } 
+
+    // Récupération de la réponse du serveur
+    const data = await response.json();
+    return data;
+} catch (error) {
+    console.log("Erreur lors de l'appel à registerUser:", error);
+    return null;
+}
+}
+
+
+
 
 
 
